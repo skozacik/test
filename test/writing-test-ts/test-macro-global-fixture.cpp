36a98e581402bfeaba884517aaae114dd4970b00
Raffi Enficiaud <raffi.enficiaud@free.fr>
2016-11-09 11:33:37 +0100

    Tests in global fixtures: checks on what is allowed
    - apparently test assertions are not possible in global fixtures
    - messages are ok

---
+++ test/writing-test-ts/test-macro-global-fixture.cpp
@@ -0,0 +1,27 @@
+//  (C) Copyright Raffi Enficiaud 2016.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/test for the library home page.
+
+// checks issue https://svn.boost.org/trac/boost/ticket/5563
+
+#define BOOST_TEST_MODULE test_macro_in_global_fixture
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+struct GlobalFixture {
+    GlobalFixture() {
+        BOOST_TEST_MESSAGE("having a message is ok");
+    }
+    ~GlobalFixture() {
+    }
+};
+
+BOOST_GLOBAL_FIXTURE( GlobalFixture );
+
+BOOST_AUTO_TEST_CASE( some_test )
+{
+    BOOST_TEST( true );
+}
