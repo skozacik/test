ad98c38fe40afb405a57e6024e72431f91bda401
Raffi Enficiaud <raffi.enficiaud@free.fr>
2016-11-11 15:56:47 +0100

    Fixing log differences on platforms

--- test/writing-test-ts/test-macro-global-fixture-failure.cpp
+++ test/writing-test-ts/test-macro-global-fixture-failure.cpp
@@ -137,6 +137,84 @@ struct guard {
     }
 };
 
+
+// this comes from the same test as log-formatter-test.cpp
+class output_test_stream_for_loggers : public output_test_stream {
+
+public:
+    explicit output_test_stream_for_loggers(
+        boost::unit_test::const_string    pattern_file_name = boost::unit_test::const_string(),
+        bool                              match_or_save     = true,
+        bool                              text_or_binary    = true )
+    : output_test_stream(pattern_file_name, match_or_save, text_or_binary)
+    {}
+
+    static std::string normalize_path(const std::string &str) {
+        const std::string to_look_for[] = {"\\"};
+        const std::string to_replace[]  = {"/"};
+        return utils::replace_all_occurrences_of(
+                    str,
+                    to_look_for, to_look_for + sizeof(to_look_for)/sizeof(to_look_for[0]),
+                    to_replace, to_replace + sizeof(to_replace)/sizeof(to_replace[0])
+              );
+    }
+
+    static std::string get_basename() {
+        static std::string basename;
+
+        if(basename.empty()) {
+            basename = normalize_path(__FILE__);
+            std::string::size_type basename_pos = basename.rfind('/');
+            if(basename_pos != std::string::npos) {
+                 basename = basename.substr(basename_pos+1);
+            }
+        }
+        return basename;
+    }
+
+    virtual std::string get_stream_string_representation() const {
+        std::string current_string = output_test_stream::get_stream_string_representation();
+
+        std::string pathname_fixes;
+        {
+            static const std::string to_look_for[] = {normalize_path(__FILE__)};
+            static const std::string to_replace[]  = {"xxx/" + get_basename() };
+            pathname_fixes = utils::replace_all_occurrences_of(
+                current_string,
+                to_look_for, to_look_for + sizeof(to_look_for)/sizeof(to_look_for[0]),
+                to_replace, to_replace + sizeof(to_replace)/sizeof(to_replace[0])
+            );
+        }
+
+        std::string other_vars_fixes;
+        {
+            static const std::string to_look_for[] = {"time=\"*\"",
+                                                      get_basename() + "(*):",
+                                                      "unknown location(*):",
+                                                      "; testing time: *us\n", // removing this is far more easier than adding a testing time
+                                                      "; testing time: *ms\n",
+                                                      "<TestingTime>*</TestingTime>",
+                                                      };
+
+            static const std::string to_replace[]  = {"time=\"0.1234\"",
+                                                      get_basename() + ":*:" ,
+                                                      "unknown location:*:",
+                                                      "\n",
+                                                      "\n",
+                                                      "<TestingTime>ZZZ</TestingTime>",
+                                                      };
+
+            other_vars_fixes = utils::replace_all_occurrences_with_wildcards(
+                pathname_fixes,
+                to_look_for, to_look_for + sizeof(to_look_for)/sizeof(to_look_for[0]),
+                to_replace, to_replace + sizeof(to_replace)/sizeof(to_replace[0])
+            );
+        }
+
+        return other_vars_fixes;
+    }
+};
+
 BOOST_AUTO_TEST_CASE( fixture_check )
 {
     guard G;
@@ -149,7 +227,7 @@ BOOST_AUTO_TEST_CASE( fixture_check )
             ? (runtime_config::save_pattern() ? PATTERN_FILE_NAME : "./baseline-outputs/" PATTERN_FILE_NAME )
             : framework::master_test_suite().argv[1] );
 
-    output_test_stream test_output( pattern_file_name, !runtime_config::save_pattern() );
+    output_test_stream_for_loggers test_output( pattern_file_name, !runtime_config::save_pattern() );
 
     test_suite* ts_0 = BOOST_TEST_SUITE( "fake root" );
     ts_0->add( BOOST_TEST_CASE( simple_check ) );
